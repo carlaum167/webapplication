976fc08f69d318c92554e7e7b31e1b39221ee250
carlaum167 <carlosgado167@gmail.com>
2025-03-21 10:53:04 -0300

    created registration page =>new.ejs|created routes post and get =>index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET new page. */
+router.get('/new', function(req, res, next) {
+  res.render('new', { title: "Customer registration", action: "/new"})
+})
+
+/*POST new page. */
+router.post('/new', function(req, res, next){
+//salvar clientes no futuro aqui
+res.redirect('/?new-true')  
+})
+
 module.exports = router;
